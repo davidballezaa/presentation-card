8ee751ebbefdbe750da55748b53a493cd88bbb45
davidballezaa <davidballezaa@gmail.com>
2022-02-04 16:52:24 -0600

    add Info jsx

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,25 @@
 import davidImage from "../images/david.jpg"
+import "../styles/Info.css"
 
 export default function Info() {
   return (
-    <img src={davidImage}/>
+    <>
+    <img src={davidImage} className="card__image"/>
+    <div className="card__info">
+      <h1>David Balleza</h1>
+      <h2>Full Stack Web Developer</h2>
+      <p>davidballezaa.github.io</p>
+      <div className="card__info--buttons">
+        <a href="" target="_blank">
+          <img src="" />
+          Email
+        </a>
+        <a href="" target="_blank">
+          <img src="" />
+         LinkedIn 
+        </a>
+      </div>
+    </div>
+    </>
   )
 }
