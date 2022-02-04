f81a5cc88dce7cb78cbc34696578aec9114cb658
davidballezaa <davidballezaa@gmail.com>
2022-02-04 17:30:10 -0600

    add hrefs to anchor tags

--- src/components/Info.js
+++ src/components/Info.js
@@ -11,14 +11,14 @@ export default function Info() {
       <h1 className="card__info--name">David Balleza</h1>
       <h2 className="card__info--role">Full Stack Web Developer</h2>
       <small className="card__info--site">
-        <a href="" target="_blank">davidballezaa.github.io</a>
+        <a href="https://davidballezaa.github.io/portfolio/" target="_blank">davidballezaa.github.io</a>
       </small>
       <div className="card__info--buttons">
-        <a href="" target="_blank" className="buttons--email">
+        <a href="mailto:davidballezaa@gmail.com" target="_blank" className="buttons--email">
           <img src={mailIcon} />
           <span>Email</span>
         </a>
-        <a href="" target="_blank" className="buttons--linkedin">
+        <a href="https://www.linkedin.com/in/davidballezaa/" target="_blank" className="buttons--linkedin">
           <img src={linkedinIcon} />
           <span>LinkedIn</span>
         </a>
