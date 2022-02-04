7207a70ae83b26d2864586a42cda0e81da9eac65
davidballezaa <davidballezaa@gmail.com>
2022-02-04 17:25:46 -0600

    add descriptive classes

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,6 @@
 import davidImage from "../images/david.jpg"
+import mailIcon from "../images/mail-icon.png"
+import linkedinIcon from "../images/linkedin-icon.png"
 import "../styles/Info.css"
 
 export default function Info() {
@@ -6,17 +8,19 @@ export default function Info() {
     <>
     <img src={davidImage} className="card__image"/>
     <div className="card__info">
-      <h1>David Balleza</h1>
-      <h2>Full Stack Web Developer</h2>
-      <p>davidballezaa.github.io</p>
+      <h1 className="card__info--name">David Balleza</h1>
+      <h2 className="card__info--role">Full Stack Web Developer</h2>
+      <small className="card__info--site">
+        <a href="" target="_blank">davidballezaa.github.io</a>
+      </small>
       <div className="card__info--buttons">
-        <a href="" target="_blank">
-          <img src="" />
-          Email
+        <a href="" target="_blank" className="buttons--email">
+          <img src={mailIcon} />
+          <span>Email</span>
         </a>
-        <a href="" target="_blank">
-          <img src="" />
-         LinkedIn 
+        <a href="" target="_blank" className="buttons--linkedin">
+          <img src={linkedinIcon} />
+          <span>LinkedIn</span>
         </a>
       </div>
     </div>
