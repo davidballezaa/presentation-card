fc1d0f7250e01b13197f3f3e25db398abc311a0e
davidballezaa <davidballezaa@gmail.com>
2022-02-05 08:29:50 -0600

    add Skills component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Info from "./components/Info"
 import Footer from "./components/Footer"
+import Skills from "./components/Skills";
 
 function App() {
   return (
-    <main className="card">
+    <div className="card">
       <Info />
+      <main>
+        <Skills />
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 }
 
