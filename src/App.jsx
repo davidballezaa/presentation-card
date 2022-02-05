9c86c3f5611f055ede456ca6acf7e8b37c2a3e76
davidballezaa <davidballezaa@gmail.com>
2022-02-04 18:11:12 -0600

    change file extension to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Info from "./components/Info"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <main className="card">
       <Info />
+      <Footer />
     </main>
   );
 }
