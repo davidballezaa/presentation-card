d083362729db8f4d4a554e611a3e0bc0430bff39
davidballezaa <davidballezaa@gmail.com>
2022-02-05 08:28:45 -0600

    add skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,15 @@
+import "../styles/Skills.css"
+
+export default function Skills() {
+  return (
+    <div className="skills">
+      <h2 className="skills__title">Skills</h2>
+      <ul className="skills__list">
+        <li>HTML</li>
+        <li>CSS, Bootstrap, SASS</li>
+        <li>Javascript, React</li>
+        <li>PHP, Laravel</li>
+      </ul>
+    </div>
+  )
+}
