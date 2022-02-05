8ce31cafbd3c74d99f93224d7645e7c6c2f6db6c
davidballezaa <davidballezaa@gmail.com>
2022-02-04 18:19:44 -0600

    finish footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import "../styles/Footer.css"
+import twitterIcon from "../images/twitter-icon.png"
+import facebookIcon from "../images/facebook-icon.png"
+import instagramIcon from "../images/instagram-icon.png"
+import githubIcon from "../images/github-icon.png"
+
 export default function Footer() {
   return (
     <footer>
-      <a href=""></a>
+      <a href="https://twitter.com/DavidBallezaA" target="_blank">
+        <img src={twitterIcon}/>
+      </a>
+      <a href="">
+        <img src={facebookIcon}/>
+      </a>
+      <a href="https://www.instagram.com/davidballezaa/" target="_blank">
+        <img src={instagramIcon}/>
+      </a>
+      <a href="https://github.com/davidballezaa" target="_blank">
+        <img src={githubIcon}/>
+      </a>
     </footer>
   )
 }
